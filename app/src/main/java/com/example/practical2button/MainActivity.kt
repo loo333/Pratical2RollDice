1b6f1c1980e19713085a22c6e680ad1d3e8117f6
Loo Ming Yea <loomy-wp18@student.tarc.edu.my>
2021-02-02 12:27:11 +0800

    init

---
+++ app/src/main/java/com/example/practical2button/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.practical2button
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+    }
+}
+fun main() {
+    val myFirstDice = Dice(6)
+    val diceRoll = myFirstDice.roll()
+    println("Your ${myFirstDice.numSides} sided dice rolled ${diceRoll}!")
+
+    val mySecondDice = Dice(20)
+    println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
+}
+
+class Dice (val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
